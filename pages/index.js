55b923aaf3a116728ce13fafecf4c3e713446a58
vikash <vikashraj144@gmail.com>
2019-08-06 16:34:11 +0530

    Fetching Data for Pages

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show._id}>
+          <Link href="/p/[id]" as={`/p/${show._id}`}>
+            <a>{show.email}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
-      </ul>
-    </Layout>
-  );
-}
+Index.getInitialProps = async function() {
+  const res = await fetch('http://localhost:8080/user');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry)
+  };
+};
+
+export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,21 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-  const router = useRouter();
+const Post = props => (
+  <Layout>
+    <h1>{props.data.firstName}</h1>
+    <p>{props.data.lastName} Age of {props.data.age}</p>
+  </Layout>
+);
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  );
-}
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`http://localhost:8080/userbyid/${id}`);
+  const show = await res.json();
+  let data = show.data;
+  console.log(`Fetched show: ${data.email}`);
+
+  return { data };
+};
+
+export default Post;
