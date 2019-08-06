2c43b52bdfd692db03ad489e5d62ac838fe58269
vikash <vikashraj144@gmail.com>
2019-08-06 11:37:44 +0530

    Using Shared Components

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
+import Layout from '../components/MyLayout';
+
 export default function About() {
-    return (
-      <div>
-        <p>This is the about page</p>
-      </div>
-    );
-  }
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  );
+}
