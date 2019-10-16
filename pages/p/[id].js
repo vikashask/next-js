57bf07c1ad2f9e63d11ac0c9c05b40204f81f343
vikash <vikashraj144@gmail.com>
2019-10-16 16:15:45 +0530

    ui changes

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,21 +1,35 @@
+import { useRouter } from 'next/router';
+import Markdown from 'react-markdown';
 import Layout from '../../components/MyLayout';
-import fetch from 'isomorphic-unfetch';
 
-const Post = props => (
-  <Layout>
-    <h1>{props.data.firstName}</h1>
-    <p>{props.data.lastName} Age of {props.data.age}</p>
-  </Layout>
-);
+export default () => {
+  const router = useRouter();
+  return (
+    <Layout>
+      <h1>{router.query.id}</h1>
+      <div className="markdown">
+        dsadsd
+      </div>
+      <style jsx global>{`
+        .markdown {
+          font-family: 'Arial';
+        }
 
-Post.getInitialProps = async function(context) {
-  const { id } = context.query;
-  const res = await fetch(`http://localhost:8080/userbyid/${id}`);
-  const show = await res.json();
-  let data = show.data;
-  console.log(`Fetched show: ${data.email}`);
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
 
-  return { data };
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </Layout>
+  );
 };
-
-export default Post;
