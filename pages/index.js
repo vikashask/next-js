789edc5c0f28ab1ffc64ce23e18db2b28420137e
vikash <vikashraj144@gmail.com>
2019-10-16 16:15:31 +0530

    some ui changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,75 @@
-// This is the Link API
+import Layout from '../components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => (
-  <div>
-    <Link href="/about">
-      <a>About Page</a>
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${post._id}`}>
+      <a>{post.email}</a>
     </Link>
-    <p>Hello Next.js</p>
-  </div>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
 );
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <PostLink key={show._id} post={show} />
+      ))}
+    </ul>
+    <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('http://localhost:8080/user');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry)
+  };
+};
 
 export default Index;
